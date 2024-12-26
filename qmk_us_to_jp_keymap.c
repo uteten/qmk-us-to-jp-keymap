de28728b5c86dd69c221406e422ff9c72b6aa9bd
u10 <github.com20190220@uten.jp>
2024-12-26 09:06:47 +0900

    呼び出しやすいようにconvert_us_to_jp_keymap(keycode)を追加

--- qmk_us_to_jp_keymap.c
+++ qmk_us_to_jp_keymap.c
@@ -1,5 +1,27 @@
-
-// QMK用のUS→JPキーマップ変換
+/**
+ * @file us_to_jp_keymap.c
+ * @brief US配列キーボードのキーコードをJP配列キーボードのキーコードに変換するためのマッピング処理を実装したファイルです。
+ *
+ * ## 利用方法:
+ * 以下のように、`process_record_user` 関数内に組み込んで使用します。
+ *
+ * @code
+ * #include "qmk_us_to_jp_keymap.c"  // US→JPキー変換用のヘッダーをインクルード
+ *
+ * bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+ *     if (record->event.pressed) {
+ *         // US→JPキー変換を実行。変換された場合はここで処理を終了
+ *         if (!convert_us_to_jp_keymap(keycode)) return false;
+ *
+ *         // その他のキー処理を記述
+ *         switch (keycode) {
+ *             // ...
+ *         }
+ *     }
+ *     return true; // 他の処理を継続
+ * }
+ * @endcode
+ */
 
 typedef struct {
     bool is_shift_required;
@@ -14,19 +36,19 @@ typedef struct {
 // Mapping table from US keyboard keys to JP keyboard keys
 // Each entry specifies how a key behaves with or without shift pressed
 const KeyMappingEntry us_to_jp_keymap[] = {
-    {KC_2,    {{false, KC_NO}, {false, KC_LBRC}}},
-    {KC_6,    {{false, KC_NO}, {false, KC_EQL}}},
-    {KC_7,    {{false, KC_NO}, {true,  KC_6}}},
-    {KC_8,    {{false, KC_NO}, {true,  KC_QUOT}}},
-    {KC_9,    {{false, KC_NO}, {true,  KC_8}}},
-    {KC_0,    {{false, KC_NO}, {true,  KC_9}}},
-    {KC_MINS, {{false, KC_NO}, {true,  KC_INT1}}},
-    {KC_SCLN, {{false, KC_NO}, {false, KC_QUOT}}},
-    {KC_EQL,  {{true,  KC_MINS},{true,  KC_SCLN}, }},
+    {KC_2,    {{false, KC_NO},   {false, KC_LBRC}}},
+    {KC_6,    {{false, KC_NO},   {false, KC_EQL}}},
+    {KC_7,    {{false, KC_NO},   {true,  KC_6}}},
+    {KC_8,    {{false, KC_NO},   {true,  KC_QUOT}}},
+    {KC_9,    {{false, KC_NO},   {true,  KC_8}}},
+    {KC_0,    {{false, KC_NO},   {true,  KC_9}}},
+    {KC_MINS, {{false, KC_NO},   {true,  KC_INT1}}},
+    {KC_SCLN, {{false, KC_NO},   {false, KC_QUOT}}},
+    {KC_EQL,  {{true,  KC_MINS}, {true,  KC_SCLN}}},
     {KC_LBRC, {{false, KC_RBRC}, {true, KC_RBRC}}},
-    {KC_RBRC, {{false,  KC_BSLS}, { true, KC_BSLS}}},
-    {KC_BSLS, {{false,  KC_INT3}, {true, KC_INT3}}},
-    {KC_QUOT, {{false, KC_COMM}, {true,  KC_2},   }},
+    {KC_RBRC, {{false, KC_BSLS}, { true, KC_BSLS}}},
+    {KC_BSLS, {{false, KC_INT3}, {true, KC_INT3}}},
+    {KC_QUOT, {{false, KC_COMM}, {true,  KC_2}}},
     {KC_GRV,  {{true,  KC_LBRC}, {true,  KC_EQL}}},
 };
 
@@ -89,23 +111,9 @@ bool process_key_mapping(int mapping_index) {
     }
 }
 
-/* 利用例
-#include "qmk_us_to_jp_keymap.c"
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    if (record->event.pressed) {
-        // ==== ↓追加 ====
-        int index = find_key_mapping_index(keycode);
-        if (index >= 0) {
-            return process_key_mapping(index);
-        }
-        // ==== ↑追加 ====
-
-        switch (keycode) {
-          //  .....
-        }
+bool convert_us_to_jp_keymap(uint16_t keycode) {
+    int mapping_index = find_key_mapping_index(keycode);
+    if (mapping_index >= 0) {
+        return process_key_mapping(mapping_index);
     }
-
-    return true; // # 他の処理を継続
-}
-*/
+    return true; // 他の処理を継n         
